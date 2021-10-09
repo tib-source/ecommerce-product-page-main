558c125a9fa8a4f616ed40cf230ccfda425518a7
Tsquare25 <tibebe1234t@gmail.com>
2021-10-09 21:07:05 +0100

    javascrip menu done

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,20 @@
-console.log('Hello, World!')
+const closeIcon = document.querySelector('#close')
+const menuIcon = document.querySelector('#menu')
+const menuItems = document.querySelector('#menuItems')
+menuItems.style.display = "none"
+
+
+menuIcon.addEventListener('click', () => {
+  curr = menuItems.style.display = "none"
+  if (curr == 'none') {
+    return menuItems.style.display = 'flex'
+  }
+})
+closeIcon.addEventListener('click', () => {
+  curr = menuItems.style.display = "none"
+  if (curr == 'flex') {
+    return menuItems.style.display = 'none'
+  }
+})
+
+
